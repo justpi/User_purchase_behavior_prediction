4212f280691e06826aeff008b712c5a79ac3ff48
justpi <36996522+justpi@users.noreply.github.com>
2020-12-01 21:46:08 +0800

    训练的特征需要调整一下，将user_id和product_id去掉，另外最后预测的时候还有bug

--- data_process.py
+++ data_process.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 
 
@@ -40,7 +41,7 @@ def cleaner(df):
     #category_code
     # na
     #brand
-    df['brand'].fillna('Not_brand', inplace=True)
+    df['brand'].fillna('not_brand', inplace=True)
     #price
     # 这些价格中有500余份为0，还有两个产品为负，分别是5716855和5716857，这两个产品只有两份记录，分别是4448和14302买的，先不做处理
     #user_id
@@ -82,14 +83,14 @@ def construct_feature(df):
     # # 方法一：按列构造
     # # No.1 product_id和user_id   这两个不算特征，加入便于索引
     #
-    # # No.2 category_id
-    # # category为数值类型，但其值过大，将其缩小1e-16
-    # df['category_id'] = df['category_id'] * 1e-16   #此处有类型转换 int64 -> float64
+    # No.2 category_id
+    # category为数值类型，但其值过大，将其缩小1e-16
+    df['category_id'] = df['category_id'] * 1e-16   #此处有类型转换 int64 -> float64
     #
-    # # No.3 price
-    # # 连续数值类型，做一下特征缩放就ok
-    # # df['price'] = (df['price'] - df['price'].mean())/(df['price'].max() - df['price'].min())    #最大最小缩放
-    # df['price'] = (df['price'] - df['price'].mean()) / df['price'].std()    #标准归一化
+    # No.3 price
+    # 连续数值类型，做一下特征缩放就ok
+    # df['price'] = (df['price'] - df['price'].mean())/(df['price'].max() - df['price'].min())    #最大最小缩放
+    df['price'] = (df['price'] - df['price'].mean()) / df['price'].std()    #标准归一化
     # # No.4 brand
     # # 字符类型，需做one-hot编码
     # df = df.join(pd.get_dummies(df.brand))
@@ -110,27 +111,109 @@ def construct_feature(df):
     dataset = pd.DataFrame(None, columns=['user_id', 'product_id', 'category_id', 'price', 'brand', 'continu_visit',
                                           'stay_time', 'view', 'cart', 'remove_from_cart', 'pre_1_product_behav',
                                           'pre_1_product_smi', 'pre_2_product_behav', 'pre_2_product_smi',
-                                          'pre_3_product_behav', 'pre_3_product_smi'],
-                           index=[i for i in range(len(df['product_id'].unique()))])
+                                          'pre_3_product_behav', 'pre_3_product_smi', 'target'],
+                           index=[i for i in range(len(df['product_id']))])
     df1 = df.groupby(['user_id', 'product_id'])
 
     k = 0   #设置一个指示数，用于指示数据在表中存放的位置
     for user in df['user_id'].unique():
-        pros = df['product_id'].loc[df['user_id'] == user]
-        #No.1
-        dataset.iloc[k]['user_id'] = user
+        pros = df['product_id'].loc[df['user_id'] == user].unique()
+
+
         for pro in pros:
             sub_data = df1.get_group((user, pro))
             #构建商品特征
+            # No.1 -- 5
+            dataset.iloc[k]['user_id'] = user
             dataset.iloc[k]['product_id'] = pro
-            # for i, row in sub_data.iterrows():  #构建每一个user_id和product_id下的用户行为特征
+            dataset.iloc[k]['category_id'] = list(df[df['product_id'] == pro].category_id)[0]
+            dataset.iloc[k]['price'] = list(df[df['product_id'] == pro].price)[0]
+            dataset.iloc[k]['brand'] = list(df[df['product_id'] == pro].brand)[0]
+            #No.6 continu_visit 需要对比前后两行session是否相同
+            dataset.iloc[k]['continu_visit'] = 0    #初始化为0,下同
+            #No.7 停留时间
+            dataset.iloc[k]['stay_time'] = 0
+            #No.8 view的次数
+            dataset.iloc[k]['view'] = 0
+            # No.9 cart的次数
+            dataset.iloc[k]['cart'] = 0
+            # No.10 remove_from_cart的次数
+            dataset.iloc[k]['remove_from_cart'] = 0
+            # No.11 pre_1_behav
+            dataset.iloc[k]['pre_1_behav'] = None
+            dataset.iloc[k]['pre_2_behav'] = None
+            dataset.iloc[k]['pre_3_behav'] = None
+            #No.12 target
+            dataset.iloc[k]['target'] = 0
+
+            start_time = None
+            end_time = None
+            sub_data.reset_index(inplace=True)
+            # print(sub_data)
+            for i, row in sub_data.iterrows():  #构建每一个user_id和product_id下的用户行为特征
+                #stay time 计算
+                if i == 0:
+                    start_time = row['event_time']
+                    end_time = row['event_time']
+                if row['event_type'] != 'purchase':
+                    end_time = row['event_time']
+                    # tartget赋值
+                    dataset.iloc[k]['target'] = 1
+                #view,cart, remove_from_cart计算
+                if row['event_type'] == 'view':
+                    dataset.iloc[k]['view'] = dataset.iloc[k]['view'] + 1
+                if row['event_type'] == 'cart':
+                    dataset.iloc[k]['cart'] = dataset.iloc[k]['cart'] + 1
+                if row['event_type'] == 'remove_from_cart':
+                    dataset.iloc[k]['remove_from_cart'] = dataset.iloc[k]['remove_from_cart'] + 1
 
 
 
-            k = k + 1   #此处还有一个bug
+
+            dataset.loc[k]['stay_time'] = (end_time - start_time).total_seconds()
 
 
 
+            k = k + 1
+    dataset.dropna(axis=0, how='all', inplace=True) #删除nan的行
+
+    # 字符列onehot编码
+    dataset = dataset.join(pd.get_dummies(dataset.brand))
+
+    #先删除部分未特征处理的列
+    dataset.drop(axis=1, labels=['brand','continu_visit', 'pre_1_product_behav', 'pre_1_product_smi', 'pre_2_product_behav',
+                                 'pre_2_product_smi', 'pre_3_product_behav', 'pre_3_product_smi'], inplace=True)
+
+    #类型转换
+    dataset = dataset.astype('float64')
+    dataset[['user_id', 'product_id']] = dataset[['user_id', 'product_id']].astype('int')
 
     return dataset
 
+
+def mysubmission(data, origindata):
+    """
+    找到除用户已经购买的产品外最可能购买的产品
+    :param data: 每个用户的每个产品购买概率预测结果
+    :param origindata: 原始数据
+    :return: 每个用户最可能购买哪个产品
+    """
+
+
+    submission = pd.DataFrame(data['user_id'].unique(), columns=['user_id'])
+    submission['product_id']= None
+    group_data = data.groupby(['user_id'])
+
+    for user in data['user_id'].unique():
+
+        product_data = group_data.get_group(user)
+        origin_pro_data = origindata['product_id'].loc[(origindata['user_id'] == user) & (origindata['event_type'] != 'purchase')]
+        res_data = product_data[product_data['product_id'].isin(origin_pro_data)]
+        print(res_data)
+        res_data.sort_values(by='purchase_1', ascending=False, inplace=True)
+        submission['product_id'].loc[submission['user_id'] == user] = res_data['product_id']
+
+    print(submission.head())
+    print(submission.describe())
+    # submission['product_id'] = submission['product_id'].astype('int')
+    return submission
--- main.py
+++ main.py
@@ -1,26 +1,82 @@
 import pandas as pd
 import data_process
+import mymodels
+from sklearn.model_selection import train_test_split
 
 
-
+import warnings
+warnings.filterwarnings("ignore")
 trainfile = './data/train.csv'
 testfile = './data/test.csv'
 
 if __name__ == '__main__':
 
     # 1. 导入数据
+    print('=================1. 导入数据=================')
     traindata = pd.read_csv(trainfile)
     testdata = pd.read_csv(testfile)
+    # print('原始训练集大小：%s, 原始测试集大小： %s' %(traindata.shape, testdata.shape))
+    #
+    # # 2. 数据处理
+    # print('=================2. 数据处理=================')
+    # traindata = traindata.merge(testdata,how='outer')
+    # print('合并后数据集大小: ', traindata.shape)
+    # # 2.1 数据信息查看
+    # # 训练集
+    # # print("=============== train set ===============")
+    # # data_process.basic_eda(traindata)
+    # # #测试集
+    # # print("=============== test set ===============")
+    # # data_process.basic_eda(testdata)
+    # # 2.2 数据清洗
+    # print('=================2.2 数据清洗=================')
+    # traindata = data_process.cleaner(traindata)
+    # # 2.3 构造数据集
+    # print('=================2.3 构造数据集=================')
+    # trainset = data_process.construct_feature(traindata)
+    # trainset.to_csv('./data/features.csv')
+    # 2.4 分割数据集
+    trainset = pd.read_csv('./data/features.csv',)
+    trainset.drop(columns=['Unnamed: 0'],axis=1,inplace=True)
+    trainset['user_id'] = trainset['user_id'].astype('int')
+    trainset['product_id'] = trainset['product_id'].astype('int')
+    #设置分割点
+    print('=================2.4 分割数据集=================')
+    split_point = trainset.loc[trainset['user_id'] == 53978].index.tolist()[0]
+    print('分割点为：', split_point)
+    testset = trainset.iloc[split_point:] #测试集
+    trainset = trainset.iloc[:split_point]    #训练集和验证集
+    print('训练集大小: %s ,测试集大小：%s' %(trainset.shape, testset.shape))
+    train_X = trainset.drop(axis=1, labels=['target'])
+    train_Y = trainset['target']
+    x_train, x_val, y_train, y_val = train_test_split(train_X,train_Y, test_size=0.3)
 
-    # 2. 数据处理
 
-    # 2.1 数据信息查看
-    # 训练集
-    print("=============== train set ===============")
-    data_process.basic_eda(traindata)
-    #测试集
-    print("=============== test set ===============")
-    data_process.basic_eda(testdata)
-    # 2.2 数据清洗
-    traindata = data_process.cleaner(traindata)
-    # 2.3 构造数据集
+    # 3. 拟合模型
+    print('=================3 数据拟合=================')
+    # 3.1 随机森林尝试
+    clf = mymodels.RandomFmodel(x_train, y_train, x_val, y_val)
+
+
+
+
+
+
+    # 4. 结果预测
+    print('=================4 结果预测=================')
+    test_X = testset.drop(axis=1, labels=['target'])
+
+    test_y = clf.predict_proba(test_X)
+
+    test_y = pd.DataFrame(test_y, columns=['purchase_0', 'purchase_1'])
+    test_y['user_id'] = test_X['user_id']
+    sub = test_X
+    # print(test_y['purchase_1'].shape)
+    sub['purchase_1'] = None
+    sub['purchase_1'].iloc[:] = test_y['purchase_1'].tolist()
+    print(sub)
+    submission = data_process.mysubmission(sub, testdata)
+    submission.to_csv('./data/submission.csv')
+    print('================= 完成 =================')
+
+
